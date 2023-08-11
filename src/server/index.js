d1bb57b2418f429dc2012dfdd300bd5b70c85bc4
zeley26 <130606421+zeley26@users.noreply.github.com>
2023-08-11 16:43:24 -0400

    build

--- src/server/index.js
+++ src/server/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.static('../../dist/'));
 
 app.get("/", (req, res) => {
-    res.sendFile( __dirname + "../../dist/index.html");
+    res.sendFile( __dirname + "/build/index.html");
 });
 
 app.post("/", async(req, res) => {
